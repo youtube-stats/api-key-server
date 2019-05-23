45048de21bfd022ab20aa8ae7e289116d523580c
Salvador Guzman <guzmansalv@gmail.com>
2019-05-22 18:48:46 -0700

    move functions into main

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
 extern crate actix_web;
 use actix_web::{http, server, App, Path, Responder};
 
-
-fn index_get(_info: Path<()>) -> impl Responder {
-    let string: &str = "GET";
-
-    println!("{}", string);
-    format!("{}", string)
-}
-
-fn index_add(info: Path<(String)>) -> impl Responder {
-    let string: &str = "ADD";
-
-    println!("{} endpoint - adding {}", string, info.into_inner());
-    format!("{}", string)
-}
-
-fn index_del(info: Path<(String)>) -> impl Responder {
-    let string: &str = "DEL";
-
-    println!("{} endpoint - deleting {}", string, info.into_inner());
-    format!("{}", string)
-}
-
 fn main() {
+    fn index_get(_info: Path<()>) -> impl Responder {
+        let string: &str = "GET";
+
+        println!("{}", string);
+        format!("{}", string)
+    }
+
+    fn index_add(info: Path<(String)>) -> impl Responder {
+        let string: &str = "ADD";
+
+        println!("{} endpoint - adding {}", string, info.into_inner());
+        format!("{}", string)
+    }
+
+    fn index_del(info: Path<(String)>) -> impl Responder {
+        let string: &str = "DEL";
+
+        println!("{} endpoint - deleting {}", string, info.into_inner());
+        format!("{}", string)
+    }
+
     server::new(
         || App::new()
             .resource("/get", |r| r.method(http::Method::GET).with(index_get))
