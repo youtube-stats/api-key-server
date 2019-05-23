605a9be03139775cbe8c70afd1201c553e84391c
Salvador Guzman <guzmansalv@gmail.com>
2019-05-22 21:29:07 -0700

    fix stuff - add len path

--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,32 @@ lazy_static! {
 
 fn index_get(_info: Path<()>) -> impl Responder {
     let mut rng: rand::prelude::ThreadRng = rand::prelude::thread_rng();
-    let keys: Vec<String> = KEYS.lock().unwrap();
+    let keys: Vec<String> = KEYS.lock().unwrap().to_vec();
     let string: String = keys.choose(&mut rng).unwrap().clone();
 
     println!("{}", string);
-    format!("{}\n", string)
+    format!("{}", string)
+}
+
+fn index_len(_info: Path<()>) -> impl Responder {
+    let len: usize = KEYS.lock().unwrap().to_vec().len();
+
+    println!("LEN");
+    format!("{}", len)
 }
 
 fn index_add(info: Path<(String)>) -> impl Responder {
     let value: String = info.into_inner();
-    let mut keys: Vec<String> = KEYS.lock().unwrap();
+    let mut keys: Vec<String> = KEYS.lock().unwrap().to_vec();
     keys.push(value);
 
-    println!("ADD endpoint - adding {}", value);
+    println!("ADD endpoint - adding {}", keys.last().unwrap());
     format!("ADD\n")
 }
 
 fn index_del(info: Path<(String)>) -> impl Responder {
     let val: String = info.into_inner();
-    let mut xs: Vec<String> = KEYS.lock().unwrap();
+    let mut xs: Vec<String> = KEYS.lock().unwrap().to_vec();
     let index = xs.iter().position(|x| *x == val).unwrap();
     xs.remove(index);
 
@@ -45,6 +52,7 @@ fn main() {
     server::new(
         || App::new()
             .resource("/get", |r| r.method(http::Method::GET).with(index_get))
+            .resource("/len", |r| r.method(http::Method::GET).with(index_len))
             .resource("/add/{key}", |r| r.method(http::Method::GET).with(index_add))
             .resource("/del/{key}", |r| r.method(http::Method::GET).with(index_del))
     ).bind("127.0.0.1:8080")
