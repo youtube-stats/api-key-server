1f9573845ea431ff37b51a3c8ca3aa84aa0872b3
Salvador Guzman <guzmansalv@gmail.com>
2019-05-22 18:47:34 -0700

    working draft

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 extern crate actix_web;
 use actix_web::{http, server, App, Path, Responder};
 
-fn index(info: Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
+
+fn index_get(_info: Path<()>) -> impl Responder {
+    let string: &str = "GET";
+
+    println!("{}", string);
+    format!("{}", string)
+}
+
+fn index_add(info: Path<(String)>) -> impl Responder {
+    let string: &str = "ADD";
+
+    println!("{} endpoint - adding {}", string, info.into_inner());
+    format!("{}", string)
+}
+
+fn index_del(info: Path<(String)>) -> impl Responder {
+    let string: &str = "DEL";
+
+    println!("{} endpoint - deleting {}", string, info.into_inner());
+    format!("{}", string)
 }
 
 fn main() {
     server::new(
         || App::new()
-            .route("/{id}/{name}/index.html", http::Method::GET, index))
-        .bind("127.0.0.1:8080").unwrap()
+            .resource("/get", |r| r.method(http::Method::GET).with(index_get))
+            .resource("/add/{key}", |r| r.method(http::Method::GET).with(index_add))
+            .resource("/del/{key}", |r| r.method(http::Method::GET).with(index_del))
+    ).bind("127.0.0.1:8080")
+        .expect("Can not bind to port 8000")
         .run();
 }
