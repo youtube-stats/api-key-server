1d65e72b72faf4fe48968800a0258ceca020eaf9
Salvador Guzman <guzmansalv@gmail.com>
2019-05-22 21:20:45 -0700

    lazy_static init

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
 extern crate actix_web;
+
+#[macro_use]
+extern crate lazy_static;
+extern crate rand;
+
 use actix_web::{http, server, App, Path, Responder};
+use rand::seq::SliceRandom;
+
+lazy_static! {
+    static ref KEYS: std::sync::Mutex<Vec<String>> = std::sync::Mutex::new(vec![]);
+}
+
+fn index_get(_info: Path<()>) -> impl Responder {
+    let mut rng: rand::prelude::ThreadRng = rand::prelude::thread_rng();
+    let keys: Vec<String> = KEYS.lock().unwrap();
+    let string: String = keys.choose(&mut rng).unwrap().clone();
+
+    println!("{}", string);
+    format!("{}\n", string)
+}
+
+fn index_add(info: Path<(String)>) -> impl Responder {
+    let value: String = info.into_inner();
+    let mut keys: Vec<String> = KEYS.lock().unwrap();
+    keys.push(value);
+
+    println!("ADD endpoint - adding {}", value);
+    format!("ADD\n")
+}
+
+fn index_del(info: Path<(String)>) -> impl Responder {
+    let val: String = info.into_inner();
+    let mut xs: Vec<String> = KEYS.lock().unwrap();
+    let index = xs.iter().position(|x| *x == val).unwrap();
+    xs.remove(index);
+
+    println!("DEL endpoint - deleting {}", val);
+    format!("DEL\n")
+}
 
 fn main() {
-    fn index_get(_info: Path<()>) -> impl Responder {
-        let string: &str = "GET";
 
-        println!("{}", string);
-        format!("{}\n", string)
-    }
-
-    fn index_add(info: Path<(String)>) -> impl Responder {
-        let string: &str = "ADD";
-
-        println!("{} endpoint - adding {}", string, info.into_inner());
-        format!("{}\n", string)
-    }
-
-    fn index_del(info: Path<(String)>) -> impl Responder {
-        let string: &str = "DEL";
-
-        println!("{} endpoint - deleting {}", string, info.into_inner());
-        format!("{}\n", string)
-    }
 
     server::new(
         || App::new()
