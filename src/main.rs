fa6af954a3941fdf627cb514a375539602ff206c
Salvador Guzman <guzmansalv@gmail.com>
2019-05-23 01:09:09 -0700

    working global state modifier

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,35 @@
 extern crate actix_web;
+#[macro_use]
+extern crate lazy_static;
 extern crate rand;
 
-use actix_web::{http, server, App, Path, Responder};
-use rand::seq::SliceRandom;
-use std::cell::RefCell;
+use std::sync::Mutex;
 
-thread_local! {
-    pub static KEYS: RefCell<Vec<String>> = RefCell::new(vec![]);
+use actix_web::{http, server, App, Path, Responder};
+use rand::prelude::{ThreadRng, thread_rng};
+use rand::seq::SliceRandom;
+
+lazy_static! {
+    static ref KEYS: Mutex<Vec<String>> = Mutex::new(vec![]);
 }
 
 fn get_key() -> String {
-    let mut rng: rand::prelude::ThreadRng = rand::prelude::thread_rng();
+    let mut rng: ThreadRng = thread_rng();
 
-    KEYS.with(|keys: &RefCell<Vec<String>>| {
-        keys.borrow().choose(&mut rng).unwrap().clone()
-    })
+    KEYS.lock().unwrap().choose(&mut rng).unwrap().clone()
 }
 
 fn len_key() -> usize {
-    KEYS.with(|keys: &RefCell<Vec<String>>| {
-        keys.borrow().len()
-    })
+    KEYS.lock().unwrap().len()
 }
 
 fn add_key(value: String) {
-    KEYS.with(|keys: &RefCell<Vec<String>>| {
-        keys.borrow_mut().push(value)
-    })
+    KEYS.lock().unwrap().push(value)
 }
 
 fn del_key(value: String) {
-    KEYS.with(|keys: &RefCell<Vec<String>>| {
-        let index: usize = keys.borrow().iter().position(|x| *x == value).unwrap();
-        keys.borrow_mut().remove(index);
-    })
+    let index: usize = KEYS.lock().unwrap().iter().position(|x| *x == value).unwrap();
+    KEYS.lock().unwrap().remove(index);
 }
 
 fn index_get(_info: Path<()>) -> impl Responder {
@@ -54,7 +50,7 @@ fn index_add(info: Path<(String)>) -> impl Responder {
     let value: String = info.into_inner();
     add_key(value.clone());
 
-    println!("ADD endpoint - adding {}", value);
+    println!("ADD endpoint - adding {} - new size {}", value, len_key());
     format!("ADD {}\n", value)
 }
 
@@ -67,15 +63,13 @@ fn index_del(info: Path<(String)>) -> impl Responder {
 }
 
 fn main() {
-
-
     server::new(
         || App::new()
-            .resource("/get", |r| r.method(http::Method::GET).with(index_get))
-            .resource("/len", |r| r.method(http::Method::GET).with(index_len))
-            .resource("/add/{key}", |r| r.method(http::Method::GET).with(index_add))
-            .resource("/del/{key}", |r| r.method(http::Method::GET).with(index_del))
-    ).bind("127.0.0.1:8080")
+            .route("/get", http::Method::GET, index_get)
+            .route("/len", http::Method::GET, index_len)
+            .route("/add/{key}", http::Method::GET, index_add)
+            .route("/del/{key}", http::Method::GET, index_del))
+        .bind("127.0.0.1:8080")
         .expect("Can not bind to port 8000")
         .run();
 }
